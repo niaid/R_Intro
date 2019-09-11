5f9d8d28557408e7b2197c16347fe0ef1b6d94ec
pooranibcbb <42645335+pooranibcbb@users.noreply.github.com>
2019-09-10 20:45:17 -0600

    add package install script and update README

---
+++ package_install.R
@@ -0,0 +1,14 @@
+# ipak function: install and load multiple R packages.
+# check to see if packages are installed. Install them if they are not, then load them into the R session.
+# adapted from: https://gist.github.com/stevenworthington/3178163
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+# usage
+packages <- c("tidyverse", "shiny", "knitr", "rmarkdown")
+ipak(packages)
+
+
