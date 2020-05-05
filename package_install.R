e21c5205c6029839c9cd9d9b0b8f6a5ddaa61dd3
Jingwen Gu <jingwen.gu@nih.gov>
2020-05-05 16:06:24 -0400

    Update package_install.R

--- package_install.R
+++ package_install.R
@@ -8,7 +8,7 @@ ipak <- function(pkg){
   sapply(pkg, require, character.only = TRUE)
 }
 # usage
-packages <- c("tidyverse", "shiny", "knitr", "rmarkdown")
+packages <- c("tidyverse", "shiny", "knitr", "rmarkdown","readr","dplyr", "Stat2Data")
 ipak(packages)
 
 
