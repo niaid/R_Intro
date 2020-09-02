ec250d2dc3f75a91ea677b4c1c1f20f9eb0025b7
pooranibcbb <42645335+pooranibcbb@users.noreply.github.com>
2020-09-02 11:18:18 -0400

    update notes

--- plotting_notes.R
+++ plotting_notes.R
@@ -1,48 +1,10 @@
-#' ## Heatmap
+#' load library
+library(dplyr) 
 
-#' uncomment to install, and then re-comment afterward.
-# install.packages("pheatmap")
-library(pheatmap)
-library(readr)
-
-#' read in Transcripts per Million output of `analyzeRepeats.pl` from HOMER.
-tpm <- read_tsv("output_rna_clean.txt")
-colnames(tpm)
-?pheatmap
-pheatmap(tpm[,9:12], show_rownames = F, scale = 'row')
-
-#' *tpm* has ~16k rows!  So takes some time.  Uncomment line below to save heatmap as pdf.
-nrow(tpm)
-#pheatmap(tpm[,9:12], show_rownames = F, scale = 'row', filename="allheatmap.pdf")
-
-#' read in DESeq2 output of `getDiffExpression.pl` from HOMER
-diffexp <- read_tsv("diff_rna_BM_vs_Th_clean.txt")
-diffexp10 <- diffexp[1:10,]
-diffexp10$TranscriptID
-
-#' *dplyr* filter for top 10
-library(dplyr)
-tpm10 <- filter(tpm, TranscriptID %in% diffexp10$TranscriptID)
-dim(tpm10)
-
-#' - why are there only 8?  Missing ID also not in original output_rna.txt
-setdiff(diffexp10$TranscriptID, tpm10$TranscriptID)
-
-#' - remake heatmap with gene names
-pheatmap(tpm10[,9:12], labels_row = tpm10$gene, scale='row')
-
-#' add some simple annotation to the columns
-#'
-#' - check column names
-colnames(tpm10)
-#' - create annotation data frame
-annotation_col <- data.frame(
-  Type = factor(c("BM", "BM", "Th", "Th"))
-)
-#' - make sure rows have same names as *columns* of heatmap
-rownames(annotation_col) <- colnames(tpm10)[9:12]
-
-pheatmap(tpm10[,9:12], labels_row = tpm10$gene, scale='row', annotation_col = annotation_col)
+#' read in data.  [BirdNest](https://rdrr.io/rforge/Stat2Data/man/BirdNest.html)
+data <- readr::read_csv("BirdNest.csv") %>%
+  na.omit %>%
+  mutate(Genus = stringr::str_split(Species, " ", simplify = T)[,1])
 
 
 #' ## ggplot2
@@ -58,122 +20,109 @@ library(ggplot2)
 #' 3. add "scales" which further specify the aesthetics: axes, colors, sizes
 
 #' *Aesthetics* map from variables in the data to components of the graph.
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`adj. p-value`)) + geom_point()
-
-#' For volcano plot we do -Log10 of p-value
-diffexp$newvalue <- -log10(diffexp$`adj. p-value`)
-
-#' How should we deal with these infinite values?  However we do it, we should alert the reader in the caption. Set the "Inf" values equal to 300 for plotting.
-head(diffexp$newvalue)
-head(diffexp$`adj. p-value`)
-diffexp$newvalue[1:4] <- 300
-
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=newvalue)) + geom_point()
-
-#' Better way is to add 1 to the p-values, so we avoid taking the log of 0.  This is more robust, as we don't have to check the largest value to replace `Inf`.  We can do this in all situations.
-diffexp$badlog10 <- -log10(1 + diffexp$`adj. p-value`)
-
-#' Why do we get so many zeroes?
-head(diffexp$badlog10, 20)
-#' Computing $log(1+x)$ suffers when $|x| << 1$.  `log1p` was written to increase precision of this computation.  We use it with the change of base formula to calculate $-log_{10}(1 + \textit{p-value})$:
-diffexp$`-Log10 adj. p` <- -log1p(diffexp$`adj. p-value`)/log(10)
-head(diffexp$`-Log10 adj. p`)
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`)) + geom_point()
+ggplot(data, aes(x=Nestling, y=Totcare)) + geom_point()
 
 
 #' **Scales and colors**
 #'
-#' Could also change the scale.  *Scales* change the scale (quantitative or qualitative) of an aesthetic mapping.
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`adj. p-value`)) + geom_point() + scale_y_log10()
+#' Plot log of Nestling 
+data$logNestling <- log10(data$Nestling)
+ggplot(data, aes(x=logNestling, y=Totcare)) + geom_point()
 
+#' Could also change the scale.  *Scales* change the scale (quantitative or qualitative) of an aesthetic mapping. 
+ggplot(data, aes(x=Nestling, y=Totcare)) + geom_point() + scale_x_log10()
 
-#' We can color the points based on p-value
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=`-Log10 adj. p`)) + geom_point()
-#' change the gradient scale
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=`-Log10 adj. p`)) + geom_point() + scale_color_gradient(low = "royalblue",high="goldenrod")
-#' We can color the points based on gene type
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=genetype)) + geom_point()
+#' Change the breaks
+ggplot(data, aes(x=Nestling, y=Totcare)) + geom_point() + scale_x_log10(breaks=c(8,10,15,20))
+
+#' We can color the points based on Location
+ggplot(data, aes(x=Nestling, y=Totcare, color=Location)) + geom_point()
+
+#' We can color the points based on number of eggs
+ggplot(data, aes(x=Nestling, y=Totcare, color=No.eggs)) + geom_point()
 
 #' **Q:** Why the difference in the color scales?
 #'
-#' **A:** Because we have different classes of variables.  p-value is a continuous numeric. And the genetype is a discrete character.
+#' **A:** Because we have different classes of variables.  No.eggs is a continuous numeric. And the Location is a discrete character/factor.
 #'
-#' For discrete scales, we use `scale_color_manual` to change the color.  We give one color for every distinct value of genetype in our data.
-unique(diffexp$genetype)
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=genetype)) + geom_point() + scale_color_manual(values = c("red", "blue", "green", "yellow", "purple", "brown"))
+#' For discrete scales, we use `scale_color_manual` to change the color.  We give one color for every distinct value of Location in our data. And use color scale to manually set the colors
+unique(data$Location)
+ggplot(data, aes(x=Nestling, y=Totcare, color=Location)) + geom_point() + scale_color_manual(values=c("red", "green", "yellow", "blue", "purple", "brown", "orange", "forestgreen", "turquoise"))
 
+#' Other continuous palettes we can use for gradients [hcl.colors](https://developer.r-project.org/Blog/public/2019/04/01/hcl-based-color-palettes-in-grdevices/)
+ggplot(data, aes(x=Nestling, y=Totcare, color=No.eggs)) + geom_point() + scale_color_gradientn(colors=hcl.colors(5, palette = "plasma"))
 
+#' Change limits
+ggplot(data, aes(x=Nestling, y=Totcare, color=No.eggs)) + geom_point() + scale_color_gradientn(colors=hcl.colors(5, palette = "plasma"), limits=c(5,12.5))
+
+#' The legend represents the scale, so use scale function to change some elements.
+ggplot(data, aes(x=Nestling, y=Totcare, color=No.eggs)) + geom_point() + scale_color_gradientn(colors=hcl.colors(5, palette = "plasma"), name="Number of eggs")
 
 #' **Saving plots**
 #'
 #' Save most recent plot to file
-ggsave("volcanoplot.pdf")
+ggsave("birdsnestplot.pdf")
 
 #' Can also assign variable name to plot and then save that.
-myplot <- ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=`-Log10 adj. p`)) + geom_point() + scale_color_gradient(low = "royalblue",high="goldenrod")
-ggsave("myplot.pdf", myplot)
+
 
 #' ### Long vs wide data
 
 #' ggplot expects **long data**.  Long data is where each column corresponds to a single variable, so you can match up variables to aesthetics.
 
-print(tpm10[,-c(1:7)])
-
-#' Use `gather` from tidyr to "gather together" the sample names and tpm measurements.
+#' Use `pivot_longer` from tidyr 
 library(tidyr)
 
-longtpm <- gather(tpm10, key = "sample", value = "TPM", starts_with("RNAseq"))
-head(longtpm[,-c(1:7)])
+long <- pivot_longer(data, cols = c("Nestling", "Incubate"), names_to="Caretype", values_to="Time")
 
-#' we can make a heatmap in ggplot as well, but it is not as easy to annotate or draw the dendrogram/clustering.
-ggplot(longtpm, aes(x=sample, y=gene, fill=TPM)) + geom_tile()
+#' Stacked bar graph
+ggplot(long, aes(x=Species, y=Time, fill=Caretype)) + geom_col()
 
-#' Can make cosmetic changes with `theme`.
+#' Can make cosmetic changes with `theme`. text angle, justification, size, background colors, etc
 ?theme
 ?element_text
-ggplot(longtpm, aes(x=sample, y=gene, fill=TPM)) + geom_tile() + theme(axis.text.x = element_text(angle=90))
+ggplot(long, aes(x=Species, y=Time, fill=Caretype)) + geom_col() + theme(axis.text.x = element_text(angle=90))
+
 #' Save your theme to a variable, so you can re-use it over and over.
-mytheme <- theme_minimal() + theme(axis.text.x = element_text(angle=90))
-ggplot(longtpm, aes(x=sample, y=gene, fill=TPM)) + geom_tile() + mytheme
+mytheme <- theme_minimal() + theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
+ggplot(long, aes(x=Species, y=Time, fill=Caretype)) + geom_col() + mytheme
+
+#' Unstacked barplot using position dodge or fill
+longsub <- long[1:10,]
+ggplot(longsub, aes(x=Species, y=Time, fill=Caretype)) + geom_col(position = "dodge") + mytheme
+
+#' Pie
+ggplot(longsub, aes(x=Species, y=Time, fill=Caretype)) + geom_col() + coord_polar(theta="y")
+
+#' use geom_bar which uses stat count and change theta or use geom_col position fill
+ggplot(longsub[1:2,], aes(x=0, y=Time, fill=Caretype)) + geom_col(position = "fill") + coord_polar(theta="y")
+
+#' use facets
+ggplot(longsub, aes(x=0, y=Time, fill=Caretype)) + geom_col(position = "fill") + coord_polar(theta="y") + facet_wrap(facets = vars(Species))
+
+#' Boxplot or Violin plot with points (remove outliers)
+ggplot(long, aes(x=Nesttype, y=Totcare)) + geom_boxplot() + mytheme
+
 
 #' ### More ways to plot
 
-
-#' Histogram of p-values
-ggplot(diffexp, aes(x=`adj. p-value`)) + geom_histogram(binwidth = 0.01, fill="powderblue", color="grey30") + mytheme
-
 #' Can convert continuous scale to discrete scale by converting numerics to *factors*.
-ggplot(diffexp[1:10,], aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=as.factor(`-Log10 adj. p`))) + geom_point() + mytheme
+ggplot(data, aes(x=Nestling, y=Totcare, color=No.eggs)) + geom_point() + scale_color_hue()
 
-#' ^ is silly example.  But makes more sense with fewer values or by *binning*.  Can use `cut`.
-diffexp$plabel <- cut(diffexp$`-Log10 adj. p`, breaks=c(-0.31,-0.1,0), include.lowest = T)
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=plabel)) + geom_point() + mytheme
+#' ^ is silly example.  But makes more sense with fewer values or by *binning*.  Can use `cut`. Careful of breaks and labels and right left interval.
+data$egglabel <- cut(data$No.eggs, breaks=c(1,5,12.5))
+ggplot(data, aes(x=Nestling, y=Totcare, color=egglabel)) + geom_point() + scale_color_hue()
 
 
-#' Label points with **ggrepel**
+#' Label points with **ggrepel** (better than geom_text)
 library(ggrepel)
-ggplot(diffexp[1:10,], aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=plabel)) + geom_point() + geom_text_repel(aes(label=gene))
+ggplot(data[1:40,], aes(x=Nestling, y=Totcare, color=egglabel)) + geom_point() + geom_text_repel(aes(label=Genus))
 
-#' label only the top 10 points by giving `geom_text_repel` its own data argument.  This will override the *data* argument of the main `ggplot` function.  So it will only use the first 10 rows of diffexp instead
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=plabel)) + geom_point() + geom_text_repel(data=diffexp[1:10,], aes(label=gene))
-#' change the color of the labels to override the `color` aesthetic in the main `ggplot` function.
-ggplot(diffexp, aes(x=`Log2 Fold Change`, y=`-Log10 adj. p`, color=plabel)) + geom_point() + geom_text_repel(data=diffexp[1:10,], aes(label=gene), color="mediumorchid", fontface="bold")
-
-
-#' ### Exercises
-
-#' 1. Plot scatterplot gene vs TPM using just the top 10 genes with each sample a different color.
-
-#' 3. Make your plots look pretty or at least suitable for publication.  Use a theme!  Increase the font size to 14.
-#' Use an existing theme (or make your own from scratch :-o with `theme`)
-?theme_bw
+#' Heatmap with pheatmap.  Can make heatmap with geom_raster/geom_tile But pheatmap is nicer.
+library(pheatmap)
+datamap <- data[,c("Incubate", "Nestling")]
+row.names(datamap) <- data$Species
+pheatmap(datamap, scale = 'column', cluster_cols = F)
 
 
-#' 2. Look up geom_line (or geom_bar) help.  Make the same plot as #1 with lines (or bars) instead of points.
-
-#' 4. Color the points based on the fold change.  Red for greater than 1, blue for less.
-
-#' 5. For heatmap using pheatmap, change annotation column colors.
-
-#' 6. Make heatmap of top 10 up-regulated and top 10 down-regulated genes. Which table(s) would you use? (Different ways to do it; one way is to use `rbind`).
 
